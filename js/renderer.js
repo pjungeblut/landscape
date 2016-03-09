a7a9bc74f48563e2e0196f7bc84cda083a565c97
Paul Jungeblut <paul.jungeblut@gmail.com>
2016-03-09 22:52:39 +0100

    Some infrastructure to compile shaders and create programs.

--- js/renderer.js
+++ js/renderer.js
@@ -23,7 +23,12 @@ Landscape.Renderer = function(canvas) {
    * @const @private {!HTMLCanvasElement} The canvas to render into.
    */
   this.canvas_ = canvas;
-  Landscape.Renderer.setUpCanvas.call(this, canvas);
+  Landscape.Renderer.setUpCanvas_.call(this);
+
+  /**
+   * @private {!WebGLRenderingContext} The WebGL rendering context.
+   */
+  this.gl_ = Landscape.Renderer.setUpWebGL_.call(this);
 };
 
 
@@ -48,17 +53,17 @@ Landscape.Renderer.cancelAnimationFrame = window.cancelAnimationFrame ||
 /**
  * Sets up the canvas for use. Handles context loss.
  *
- * @param {!HTMLCanvasElement} canvas The canvas to set up.
  * @this {!Landscape.Renderer}
+ * @private
  */
-Landscape.Renderer.setUpCanvas = function(canvas) {
+Landscape.Renderer.setUpCanvas_ = function() {
   // The GPU is a shared resource, the WebGL context can be lost. In this
   // case, everything needs to be set up again.
   // For more detail: https://www.khronos.org/webgl/wiki/HandlingContextLost
 
   // Prevent the default in this case to save the page from crashing.
   // Also stop the rendering loop to avoid errors.
-  canvas.addEventListener('webglcontextlost', function(event) {
+  this.canvas_.addEventListener('webglcontextlost', function(event) {
     event.preventDefault();
     if (this.renderingLoopId_ !== 0) {
       Landscape.Renderer.cancelAnimationFrame(this.renderingLoopId_);
@@ -66,9 +71,124 @@ Landscape.Renderer.setUpCanvas = function(canvas) {
   }.bind(this), false);
 
   // Re-setup the whole WebGL.
-  canvas.addEventListener('webglcontextrestored', function() {
-    // TODO (pjungeblut): Re-Setup everything.
+  this.canvas_.addEventListener('webglcontextrestored', function() {
+    this.gl_ = Landscape.Renderer.setUpWebGL_();
+    // TODO (pjungeblut): Rendering loop needs to be restarted.
   }, false);
+};
+
+
+/**
+ * Initializes WebGL. Gets the WebGL context, compiles shaders and creates
+ * programs.
+ *
+ * Throws an error, if the WebGL context could not be created.
+ *
+ * @return {!WebGLRenderingContext} The WebGL context.
+ * @this {!Landscape.Renderer}
+ * @private
+ */
+Landscape.Renderer.setUpWebGL_ = function() {
+  // Configuring the WebGL context.
+  var attributes = {
+    alpha: true,
+    depth: true,
+    stencil: false,
+    antialias: true,
+    premultipliedAlpha: true,
+    preserveDrawingBuffer: false
+  };
+
+  // Gets the WebGL context.
+  var gl = /** @type{!WebGLRenderingContext} */ (
+      this.canvas_.getContext('webgl', attributes) ||
+      this.canvas_.getContext('experimental-webgl', attributes));
+  if (!gl) {
+    throw new Error('Could not get a WebGL context.');
+  }
+
+  // TODO (pjungeblut): Compile shaders, create programs.
+
+  return gl;
+};
+
+
+/**
+ * Creates a shader object from a script tag.
+ *
+ * The type of the script tag determines the type of the shader.
+ *   x-shader/x-vertex => Vertex Shader
+ *   x-shader/x-fragment => Fragment Shader
+ *
+ * Throws an error, if there is no script with the specified id or if the shader
+ * type could not be determined.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL context.
+ * @param {string} scriptId The id if the script tag to read the shader from.
+ * @return {!WebGLShader}
+ */
+Landscape.Renderer.createShaderFromScript = function(gl, scriptId) {
+  // Reads the shader code.
+  var shaderScript = document.getElementById(scriptId);
+  if (!shaderScript) {
+    throw new Error('Could not find script with id \'' + scriptId + '\' ' +
+        'containing shader code');
+  }
+  var shaderSource = shaderScript.text;
+
+  // Determines the shader type.
+  var shaderType;
+  if (shaderScript.type === 'x-shader/x-vertex') {
+    shaderType = gl.VERTEX_SHADER;
+  } else if (shaderScript.type === 'x-shader/x-fragment') {
+    shaderType = gl.FRAGMENT_SHADER;
+  } else {
+    throw new Error('The shader script with if \'' + scriptId + '\' does ' +
+        'not contain a valid type for the shader.');
+  }
+
+  // Compiles the shader.
+  var shader = gl.createShader(shaderType);
+  gl.shaderSource(shader, shaderSource);
+  gl.compileShader(shader);
+
+  // Checks, whether there was a problem during compilation.
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS) &&
+      !gl.isContextLost()) {
+    var error = gl.getShaderInfoLog(shader);
+    var message = 'Error compiling shader \'' + shader + '\': ' + error;
+    gl.deleteShader(shader);
+    throw new Error(message);
+  }
+
+  return shader;
+};
+
+
+/**
+ * Creates a WebGLProgram.
+ * 
+ * @param {!WebGLRenderingContext} gl The WebGL context.
+ * @param {!WebGLShader} vertexShader The vertex shader.
+ * @param {!WebGLShader} fragmentSahder The fragment shader.
+ * @return {!WebGLProgram} The program to use for the draw call.
+ */
+Landscape.Renderer.createProgram = function(gl, vertexShader, fragmentSahder) {
+  // Creates the program and attaches the shaders.
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentSahder);
+
+  // Links the program and checks for errors.
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS) && !gl.isContextLost()) {
+    var error = gl.getProgramInfoLog(program);
+    var message = 'Error linking program: ' + error;
+    gl.deleteProgram(program);
+    throw new Error(message);
+  }
+
+  return program;
 };
 
 
