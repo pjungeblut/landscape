aaf7524bf8430416c5cdd6327c9c1eea50e86e6a
Paul Jungeblut <paul.jungeblut@gmail.com>
2016-03-09 19:14:32 +0100

    Basic infrasturcture. WebGL context lost handling.

--- js/landscape.js
+++ js/landscape.js
@@ -1,18 +1,43 @@
-goog.provide('Landscape.Landscape');
+goog.provide('Landscape');
 
-
-
-/**
- * Main class for the landscape renderer.
- * Initializes the WebGL canvas and sets everything up to enable rendering.
- * @constructor
- */
-Landscape.Landscape = function() {
-  console.log('Hallo Welt');
-};
+goog.require('Landscape.Renderer');
 
 
 /**
- * @const {string} The id of the canvas to render to.
+ * @const {string} The ID of the canvas to render into.
  */
+Landscape.CANVAS_ID = 'landscapeCanvas';
 
+// Initializes the renderer and the controls.
+(function() {
+  'use strict';
+
+  /**
+   * Gets the canvas to render to from the DOM. Throws an error, if there is no
+   * canvas with ID Landscape.CANVAS_ID in the DOM.
+   *
+   * @return {!HTMLCanvasElement} The canvas to render into.
+   */
+  function getCanvas() {
+    var canvas = document.getElementById(Landscape.CANVAS_ID);
+    if (!canvas) {
+      throw new Error('Could not find canvas with id \'' +
+          Landscape.CANVAS_ID + '\' in the DOM.');
+    }
+    return /** @type {!HTMLCanvasElement} */ (canvas);
+  }
+
+  /**
+   * Initializes the renderer and the controls.
+   */
+  function initRendering() {
+    var renderer = new Landscape.Renderer(getCanvas());
+  }
+
+  // Starts everything.
+  window.addEventListener('load', function() {
+    // Start rendering.
+    initRendering();
+    console.log('Started');
+  });
+})();
---
+++ js/renderer.js
@@ -0,0 +1,62 @@
+goog.provide('Landscape.Renderer');
+
+
+
+/**
+ * Main class for the landscape renderer.
+ * Initializes the WebGL canvas and sets everything up to enable rendering.
+ *
+ * @param {!HTMLCanvasElement} canvas The canvas to render into.
+ * @constructor
+ */
+Landscape.Renderer = function(canvas) {
+  /**
+   * @private {number} The id of the rendering loop. Rendering is done in a
+   *     requestAnimationFrame loop that needs to be stopped when the context is
+   *     lost.
+   *     A value of 0 indicates that there is no active rendering loop. Any
+   *     non-zero value may be a valid id.
+   */
+  this.renderingLoopId_ = 0;
+
+  /**
+   * @const @private {!HTMLCanvasElement} The canvas to render into.
+   */
+  this.canvas_ = canvas;
+  Landscape.Renderer.setUpCanvas.call(this, canvas);
+};
+
+
+/**
+ * Sets up the canvas for use. Handles context loss.
+ *
+ * @param {!HTMLCanvasElement} canvas The canvas to set up.
+ * @this {!Landscape.Renderer}
+ */
+Landscape.Renderer.setUpCanvas = function(canvas) {
+  // The GPU is a shared resource, the WebGL context can be lost. In this
+  // case, everything needs to be set up again.
+  // For more detail: https://www.khronos.org/webgl/wiki/HandlingContextLost
+
+  // Prevent the default in this case to save the page from crashing.
+  // Also stop the rendering loop to avoid errors.
+  canvas.addEventListener('webglcontextlost', function(event) {
+    event.preventDefault();
+    if (this.renderingLoopId_ !== 0) {
+      window.cancelAnimationFrame(this.renderingLoopId_);
+    }
+  }.bind(this), false);
+
+  // Re-setup the whole WebGL.
+  canvas.addEventListener('webglcontextrestored', function() {
+    // TODO (pjungeblut): Re-Setup everything.
+  }, false);
+};
+
+
+/**
+ * Starts the rendering loop. Sets up a requestAnimationFrame loop.
+ */
+Landscape.Renderer.prototype.startRendering = function() {
+  // TODO (pjungeblut): Start the rendering loop.
+};
